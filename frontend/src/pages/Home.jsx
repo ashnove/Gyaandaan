e4d3f44cba85551790ece0ce3f8e878a727acdbd
onkar <ratnaparkhionkar@gmail.com>
2022-01-20 12:15:21 +0530

    Dropdown added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import {SingleSelectDropdown} from "../components/Dropdown"
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    Home
+    <SingleSelectDropdown />
+    </div>;
+  
 };
 
 export default Home;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import {MultipleSelectChip} from "../components/Dropdown"
+
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return 
+  <div>
+    Profile
+    <MultipleSelectChip />
+  </div>;
 };
 
 export default Profile;
