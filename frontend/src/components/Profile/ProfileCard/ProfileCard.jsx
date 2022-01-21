bf211c7a471fb7bbb0267ebbedbd467196e0b9b9
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2022-01-21 22:15:10 +0530

    card color changes

--- frontend/src/components/Profile/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/Profile/ProfileCard/ProfileCard.jsx
@@ -13,13 +13,14 @@ const ProfileCard = (props) => {
     return (
             <div className={clsname}>
                 <Card className={classes.ProfileCard + " mx-auto"} >
-                    <Card.Img className={classes.ProfileCardBackgroundImage} variant="top" src="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80" />
+                    <Card.Img className={classes.ProfileCardBackgroundImage} variant="top" src="./images/random-grey-variations"/>
                     <Card.Img className={classes.ProfileCardImage} alt="User Image" src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798"/>
-                    <Card.Body className={"text-center " + classes.ProfileCardBody}>
-                        <Card.Text className={classes.TextBold + " mb-0"}>
-                            Victor Crest <span className={classes.TextMuted + " pl-1"}>26</span>
+                    <Card.Body className={"text-center " + classes.ProfileCardBody} >
+                        <Card.Text className={classes.TextBold + " mb-0"} style={{color: "#f5f6fa"}}>
+                            Victor Crest
+                             {/* <span className={classes.TextMuted + " pl-1"} >26</span> */}
                         </Card.Text>
-                        <Card.Text className={classes.TextMuted}>
+                        <Card.Text className={classes.TextMuted} style={{color: "#dcdde1"}}>
                             London
                         </Card.Text>
 
