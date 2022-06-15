603e550eb07b4dd6eb5c7f57b1313caa79187e40
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2022-06-15 15:03:20 +0530

    PostgreSQL added

--- backend/src/main/java/com/gydn/gyandaan/Repository/TopicRepository.java
+++ backend/src/main/java/com/gydn/gyandaan/Repository/TopicRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    String GET_TOPIC_BY_NAME = "SELECT * FROM GYDNTABLE.topic WHERE topic_name = ?1";
+    String GET_TOPIC_BY_NAME = "SELECT * FROM topic WHERE topic_name = ?1";
 
     @Query(value = GET_TOPIC_BY_NAME, nativeQuery = true)
     public Topic findTopicByName(String topicName);
